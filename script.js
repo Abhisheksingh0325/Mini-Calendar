a306637f82d835bda61ce0c029d7eaf6fa6d0f5a
Abhishek Singh <Abhisheksiingh5261@gmail.com>
2023-11-02 17:42:29 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let date = document.getElementById("date");
+let day = document.getElementById("day");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+
+let today = new Date();
+const Week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+date.innerHTML = today.getDate();
+day.innerHTML = Week[today.getDay()];
+month.innerHTML = months[today.getMonth()];
+year.innerHTML = today.getFullYear();
